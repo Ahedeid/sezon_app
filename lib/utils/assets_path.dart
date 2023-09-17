cf86c6c49aad8d3e0dd47a895b6d5e509454897f
ahed <ahedeid2000@gmail.com>
2023-09-18 00:21:57 +0300

    add images and setup path , and add fonts

---
+++ lib/utils/assets_path.dart
@@ -0,0 +1,17 @@
+class AssetPath {
+  static const String baseImage = 'assets/images/';
+
+  // -------------------------- image path ------------------------------------
+  static const String authBackground = '${baseImage}auth_background.png';
+  static const String logo = '${baseImage}logo.png';
+  static const String placeholder = '${baseImage}placeholder.png';
+  static const String category = '${baseImage}categoryImage.png';
+  static const String product = '${baseImage}product.png';
+  static const String productDetails = '${baseImage}product_details.png';
+  static const String stripe = '${baseImage}stripe.svg';
+  static const String visa = '${baseImage}visa.svg';
+  static const String baseIcon = 'assets/icons/';
+  static const String addressIcon = '${baseIcon}address_icon.svg';
+  static const String paymentIcon = '${baseIcon}payment_icon.svg';
+  static const String productCharIcon = '${baseIcon}product_char_icon.svg';
+}
